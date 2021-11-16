392a132a505e0bb2a020cd335e52cf48c894a1a7
Open Close <openandclose23@gmail.com>
2021-11-17 03:40:34 +0900

    Add independent undo message test (TestBoxData)

--- tests/test_base.py
+++ tests/test_base.py
@@ -71,38 +71,44 @@ class TestBoxData:
 
         boxdata = slash._BoxData(self.create_data())
 
-        boxdata.overwrite((1, 2, 3), box1)
+        # 1
+        boxdata.overwrite((1, 2, 3), box1, msg='msg1')
         self.check(boxdata, (1, 2, 3), [box1])
         self.check_rects(boxdata, [(box1, [1, 2, 3])])
 
-        boxdata.append((2, 3, 4), box2)
+        # 2
+        boxdata.append((2, 3, 4), box2, msg='msg2')
         self.check(boxdata, (1,), [box1])
         self.check(boxdata, (2, 3), [box1, box2])
         self.check(boxdata, (4,), [box2])
         self.check_rects(boxdata, [(box1, [1, 2, 3]), (box2, [2, 3, 4])])
 
-        boxdata.clear((2, 3))
+        # 3
+        boxdata.clear((2, 3), msg='msg3')
         self.check(boxdata, (2, 3), [])
         self.check_rects(boxdata, [(box1, [1]), (box2, [4])])
 
-        boxdata.discard((1,), box2)  # noop
+        # error (can't discard non-existent box)
+        boxdata.discard((1,), box2, msg='error!')
         self.check(boxdata, (1,), [box1])
         self.check_rects(boxdata, [(box1, [1])])
         assert 'NoBoxToRemoveError: ' in capsys.readouterr().out.rstrip()
 
-        boxdata.discard((1,), box1)
+        # 4
+        boxdata.discard((1,), box1, msg='msg4')
         self.check(boxdata, (1,), [])
         self.check_rects(boxdata, [(box1, [])])  # leaving blank list
 
-        boxdata.append((4,), box2)  # now ignores the same box
+        # error (can't append a box already there)
+        boxdata.append((4,), box2, msg='error!')
         self.check(boxdata, (4,), [box2])
         self.check_rects(boxdata, [(box2, [4])])
         assert 'DuplicateBoxError: ' in capsys.readouterr().out.rstrip()
 
-        assert boxdata.undo().split()[0] == 'discard:'
-        assert boxdata.undo().split()[0] == 'clear:'
-        assert boxdata.undo().split()[0] == 'append:'
-        assert boxdata.undo().split()[0] == 'overwrite:'
+        assert boxdata.undo() == 'msg4'
+        assert boxdata.undo() == 'msg3'
+        assert boxdata.undo() == 'msg2'
+        assert boxdata.undo() == 'msg1'
         assert [b.data for b in boxdata.boxes] == [[] for _ in range(8)]
 
 class TestNumParser:
